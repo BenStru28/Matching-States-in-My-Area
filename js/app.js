c4caad52c2b0c1f48f9d2b3b61480d40c4fc0fa5
BenStru28 <Bengstru@icloud.com>
2024-08-24 02:41:54 -0400

    Create p and img elements inside card

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 const stateData = [
   { isImage: false, name: "Delaware" },
   { isImage: false, name: "Pennsylvania" },
-  { isImage: false, name: "New Jersey" },
+  { isImage: false, name: "New_Jersey" },
   { isImage: false, name: "Maryland" },
   { isImage: false, name: "Virginia" },
-  { isImage: false, name: "West Virginia" },
-  { isImage: false, name: "New York" },
+  { isImage: false, name: "West_Virginia" },
+  { isImage: false, name: "New_York" },
   { isImage: false, name: "Connecticut" },
   { isImage: true, name: "Delaware" },
   { isImage: true, name: "Pennsylvania" },
-  { isImage: true, name: "New Jersey" },
+  { isImage: true, name: "New_Jersey" },
   { isImage: true, name: "Maryland" },
   { isImage: true, name: "Virginia" },
-  { isImage: true, name: "West Virginia" },
-  { isImage: true, name: "New York" },
+  { isImage: true, name: "West_Virginia" },
+  { isImage: true, name: "New_York" },
   { isImage: true, name: "Connecticut" },
 ];
+
+let selected = "";
+
+const matches = [];
 function randomizeArray(arr) {
   return arr
     .map((value) => ({ value, sort: Math.random() }))
@@ -25,52 +29,79 @@ function randomizeArray(arr) {
 
 const randomInt = Math.floor(Math.random() * stateData.length - 1) + 1;
 
-let selected = "";
+function showContents(element) {
+  const textElement = element.querySelectorAll("p")[0];
+  if (element.id.endsWith("_img")) {
+    const imgElement = element.querySelectorAll("img")[0];
+    imgElement.style.display = "block";
+    textElement.innerHTML = "";
+  } else {
+    textElement.innerHTML = element.id.replace("_", " ");
+  }
+}
 
-const matches = [];
+function hideContents(element) {
+  const textElement = element.querySelectorAll("p")[0];
+  if (element.id.endsWith("_img")) {
+    const imgElement = element.querySelectorAll("img")[0];
+    imgElement.style.display = "none";
+    textElement.innerHTML = "?";
+  } else {
+    textElement.innerHTML = "?";
+  }
+}
 
 function clickEvent(e) {
-  if (matches.includes(e.target.id) || matches.includes(selected)) {
+  if (matches.includes(e.currentTarget.id) || matches.includes(selected)) {
     selected = "";
+    hideContents(e.currentTarget);
+    return;
   }
   if (selected.length) {
-    if (selected !== e.target.id && selected === e.target.id.split("_")[0]) {
-      document.getElementById(selected).style.backgroundColor = "blue";
-      e.target.style.backgroundColor = "blue";
-      matches.push(selected, e.target.id);
+    const selectedElement = document.getElementById(selected);
+    if (selected.split("_")[0] === e.currentTarget.id.split("_")[0]) {
+      selectedElement.style.backgroundColor = "blue";
+      e.currentTarget.style.backgroundColor = "blue";
+      matches.push(selected, e.currentTarget.id);
       selected = "";
     } else {
-      document.getElementById(selected).style.backgroundColor = "grey";
-      e.target.style.backgroundColor = "grey";
+      selectedElement.style.backgroundColor = "grey";
+      e.currentTarget.style.backgroundColor = "grey";
+      hideContents(selectedElement);
       selected = "";
     }
   } else {
-    selected = e.target.id;
-    e.target.style.backgroundColor = "red";
+    showContents(e.currentTarget);
+    selected = e.currentTarget.id;
+    e.currentTarget.style.backgroundColor = "red";
   }
 }
 
 function createCards() {
   stateData.forEach((state) => {
-    const updatedName = state.name.replace(" ", "");
+    const textElement = document.createElement("p");
+    textElement.innerHTML = "?";
+    textElement.classList.add("text");
     if (state.isImage) {
       const card = document.createElement("div");
       const img = document.createElement("img");
-      img.src = `assets/${updatedName}.png`;
+      img.src = `assets/${state.name}.png`;
       img.width = 50;
       img.height = 50;
+      img.alt = "Map of " + state.name;
       img.style.display = "none";
+      img.classList.add("img");
       card.appendChild(img);
-      card.innerHTML = "?";
+      card.appendChild(textElement);
       card.classList.add("card");
-      card.id = updatedName + "_img";
+      card.id = state.name + "_img";
       card.addEventListener("click", clickEvent);
       document.getElementById("board").appendChild(card);
     } else {
       const card = document.createElement("div");
-      card.innerHTML = "?";
+      card.appendChild(textElement);
       card.classList.add("card");
-      card.id = updatedName;
+      card.id = state.name;
       card.addEventListener("click", clickEvent);
       document.getElementById("board").appendChild(card);
     }
